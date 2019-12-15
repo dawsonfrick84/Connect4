dc30ca045317551bebefe5dba0c1703412760720
dawsonfrick84 <54998455+dawsonfrick84@users.noreply.github.com>
2019-12-15 09:39:56 -0600

    Add functionality to player turns and updating the board

--- conn4.hs
+++ conn4.hs
@@ -39,18 +39,27 @@ coords= [ [1,1,0], [1,2,0], [1,3,0], [1,4,0], [1,5,0], [1,6,0], [1,7,0],
         [5,1,0], [5,2,0], [5,3,0], [5,4,0], [5,5,0], [5,6,0], [5,7,0],
         [6,1,0], [6,2,0], [6,3,0], [6,4,0], [6,5,0], [6,6,0], [6,7,0]
         ]
-player=1
 
-main = do
-        putStrLn ("Player " ++ (toString player) ++ ": What column do you want to place?")
-        column <- inputCol
-        if column > 7 || column < 1
-        then do
-            putStrLn ("Try a different column!")
-            main
-        else do
-            let coords2 = coords
-            let coords2 = updateBoard coords player column
-            printBoard coords2
-            let player = 2
-            main
+startPlayer=1
+
+start = do
+        play coords startPlayer
+
+play board player = do
+            putStrLn ("Player " ++ (toString player) ++ ": What column do you want to place?")
+            column <- inputCol
+            if column > 7 || column < 1
+            then do
+                putStrLn ("Try a different column!")
+                play board player
+            else do
+                let board2 = board
+                let board2 = updateBoard board player column
+                printBoard board2
+                if player == 1
+                then do
+                    let player = 2
+                    play board2 player
+                else do
+                    let player = 1
+                    play board2 player
